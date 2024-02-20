8b13bfc0af7c835d5f5cb393920c9464ebd0e31e
Robin <robin.lahtinen@gmail.com>
2024-02-20 10:47:33 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import Login from './screens/Login'
+import {PaperProvider} from 'react-native-paper'
+
+export default function App() {
+    return (
+        <PaperProvider>
+            <Login/>
+        </PaperProvider>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
